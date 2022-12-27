e94a6d6128be82cde6904881f2eea6b78c329e85
유민하 <ymh22@angeluce.kr>
2022-12-27 14:48:41 +0900

    simple TodoList

---
+++ src/main/java/org/zerock/jdbcex/controller/TodoModifyController.java
@@ -0,0 +1,64 @@
+package org.zerock.jdbcex.controller;
+import lombok.extern.log4j.Log4j2;
+import org.zerock.jdbcex.dto.TodoDTO;
+import org.zerock.jdbcex.service.TodoService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Log4j2
+@WebServlet(name = "TodoModifyController", urlPatterns = "/todo/modify")
+public class TodoModifyController extends HttpServlet {
+
+    TodoService todoService = TodoService.INSTANCE;
+    private final DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+
+        long tno = Long.parseLong(req.getParameter("tno"));
+        log.info("/todo/modify GET...");
+        log.info("tno : " + tno);
+
+        try {
+            TodoDTO todoDTO = todoService.get(tno);
+            log.info("여여 : " + todoDTO);
+            req.setAttribute("dto", todoDTO);
+            req.getRequestDispatcher("/WEB-INF/todo/modify.jsp").forward(req, resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ServletException("modify GET error");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String finishedStr = req.getParameter("finished");
+
+        TodoDTO todoDTO = TodoDTO.builder()
+                .tno(Long.parseLong(req.getParameter("tno")))
+                .title(req.getParameter("title"))
+                .dueDate(LocalDate.parse(req.getParameter("dueDate"), DATEFORMATTER))
+                .finished(finishedStr != null && finishedStr.equals("on"))
+                .build();
+
+        log.info("/todo/modify POST...");
+        log.info("todoDTO : " + todoDTO);
+
+        try {
+            todoService.modify(todoDTO);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ServletException("modify POST error");
+        }
+
+        resp.sendRedirect("/todo/list");
+
+    }
+}
